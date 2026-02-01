d922f1e5d1a10e388617e0c1cb5871ec1d12fb42
joserin <games12h@gmail.com>
2026-02-01 14:37:58 -0400

    agregando supabase para obtener productos

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,21 @@
 /// <reference types="astro/client" />
 
 interface Product {
-  id: string;
-  name: string;
-  category: string;
-  description: string;
-  price: number;
-  imageUrl: string;
-  brand: string;
-  model: string;
-  // Agrega otros campos que necesites de tu JSON
+  codigo: string;
+  codigo_oem: string;
+  nombre: string;
+  categoria: string;
+  precio: number;
+  imagen_url: string;
+}
+
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: string
+  readonly SUPABASE_ANON_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
 
 interface NavActionProps {
